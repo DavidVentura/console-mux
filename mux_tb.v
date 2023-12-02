852daa6d5f548851a3aa8798cf9c717e93a05275
DavidVentura <davidventura27@gmail.com>
2023-12-02 23:13:18 +0100

    working m:n mux in tb

--- mux_tb.v
+++ mux_tb.v
@@ -1,14 +1,28 @@
 module test;
 	reg clk = 0;
-	reg [0:3] counter = 0;
-	reg [0:3] sel = 0;
 
+	// There are 16 selectors; each selector marks the source pin for a given
+	// pin.
+	// If `selectors[0] = 15`, then the PIN 0 will mirror the values seen at PIN 15
+	reg [0:3] selectors [0:15];
+
+	// There are 16 output pins, which get their value assigned
+	// from the `selectors` mapping
+	wire [0:15] out;
+
+	// There are "4" (16..) input pins, which are used to source
+	// values for `out`
+	wire [0:3] gpios = {a,b,c,d};
+
+	// This is an N to M (#gpios to #out) mux, where multiple output pins (M)
+	// can source the same source (N) pin
+
+	// Counter and a-d are used to generate test input values
+	reg [0:3] counter = 0;
 	reg a;
 	reg b;
 	reg c;
 	reg d;
-	wire out;
-	wire [0:3] gpios = {a,b,c,d};
 
 	always #1 begin
 		clk = !clk;
@@ -19,23 +33,33 @@ module test;
 		a = (counter & 4'b1000) == 4'b1000;
 	end
 
-	//assign a = 1;
-	//assign gpios = 4'b1010;
-
-	initial $monitor("%d, gpios %b, sel %d, out %d", $time, gpios, sel, out);
+	initial $monitor("%d, gpios %b, sel0 %d, sel1 %d, out0 %d, out1 %d", $time, gpios, selectors[0], selectors[1], out[0], out[1]);
 	initial begin
-		#1 sel = 0;
-		#1 sel = 1;
-		#1 sel = 2;
-		#1 sel = 3;
-		#1 sel = 0;
-		#1 sel = 3;
-		#1 sel = 0;
+		#1 selectors[1] = 1;
+
+		#1 selectors[0] = 0;
+		#1 selectors[0] = 1;
+		#1 selectors[0] = 2;
+		#1 selectors[0] = 3;
+		#1 selectors[0] = 0;
+		#1 selectors[0] = 3;
+		#1 selectors[0] = 0;
 
 
 		#10 $finish;
 	end
 
-	mux m1 (clk, gpios, sel, out);
+	genvar i;
+	generate
+		for (i=0; i<16; i=i+1) begin
+			mux m1 (clk, gpios, selectors[i], out[i]);
+		end
+	endgenerate
 
+
+initial
+begin
+   $dumpfile("test.vcd");
+   $dumpvars(0,test);
+end
 endmodule
