1f44c5bade6d96248beb2f2ff5a007987b2ff54f
DavidVentura <davidventura27@gmail.com>
2023-12-09 00:36:00 +0100

    double-buffer selectors and enabled_out to prevent applying 1-byte at a time

--- comm.v
+++ comm.v
@@ -23,7 +23,9 @@ parameter sel_width = $clog2(INPUT_COUNT)*OUTPUT_COUNT;
 wire [INPUT_COUNT-1:0] in_pins;
 wire [sel_width-1:0] selectors;
 reg  [sel_width-1:0] selectors_r = 0;
+reg  [sel_width-1:0] selectors_buf = 0;
 reg  [OUTPUT_COUNT-1:0] enabled_out_r = 0;
+reg  [OUTPUT_COUNT-1:0] enabled_out_buf = 0;
 wire [OUTPUT_COUNT-1:0] enabled_out;
 
 uart_tx #(.CLK_PER_BIT(CLOCK_PER_BIT)) 		  tx(clk, tx_data, tx_data_ready, tx_done, tx_serial_line);
@@ -37,11 +39,6 @@ wire [7:0] f_data_out;
 wire f_full;
 wire f_empty;
 fifo f(clk, f_w_en, f_adv_r, f_data_in, f_data_out, f_full, f_empty);
-
-
-// COMM state
-reg [3:0] byte_counter = 0;
-reg [3:0] state = SM_IDLE;
 
 localparam PIN_MAP_SIZE = sel_width/8; //bitfield->bytes
 localparam OUT_PIN_ENABLE_SIZE = OUTPUT_COUNT/8; //bitfield->bytes
@@ -58,6 +55,10 @@ localparam SM_OUTPUT_READ_ENABLE_MASK 		= 4'b0001;
 localparam SM_OUTPUT_READ_PIN_MAP 			= 4'b0010;
 localparam SM_OUTPUT_WRITE_ENABLE_MASK 		= 4'b0011;
 localparam SM_OUTPUT_WRITE_PIN_MAP 			= 4'b0100;
+
+// COMM state
+reg [3:0] byte_counter = 0;
+reg [3:0] state = SM_IDLE;
 
 assign selectors = selectors_r;
 assign enabled_out = enabled_out_r;
@@ -132,14 +133,18 @@ always @(posedge clk) begin
 			end
 		end
 		SM_OUTPUT_WRITE_ENABLE_MASK: begin
-			// FIXME should double buffer
-			enabled_out_r[(8*(byte_counter-1))+:8] <= rx_data_r;
-			if (byte_counter == OUT_PIN_ENABLE_SIZE) state <= SM_OUTPUT_READ_ENABLE_MASK;
+			enabled_out_buf[(8*(byte_counter-1))+:8] <= rx_data_r;
+			if (byte_counter == OUT_PIN_ENABLE_SIZE) begin
+				state <= SM_OUTPUT_READ_ENABLE_MASK;
+				@(posedge clk) enabled_out_r <= enabled_out_buf;
+			end
 		end
 		SM_OUTPUT_WRITE_PIN_MAP: begin
-			// FIXME should double buffer
-			selectors_r[(8*(byte_counter-1))+:8] <= rx_data_r;
-			if (byte_counter == PIN_MAP_SIZE) state <= SM_OUTPUT_READ_PIN_MAP;
+			selectors_buf[(8*(byte_counter-1))+:8] <= rx_data_r;
+			if (byte_counter == PIN_MAP_SIZE) begin
+				state <= SM_OUTPUT_READ_PIN_MAP;
+				@(posedge clk) selectors_r <= selectors_buf;
+			end
 		end
 	endcase
 end
