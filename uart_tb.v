7316c61b6acc4d7db7aaad3dcecbf46c8ff2ae00
DavidVentura <davidventura27@gmail.com>
2023-12-09 14:56:02 +0100

    small cleanup

--- uart_tb.v
+++ uart_tb.v
@@ -16,30 +16,30 @@ parameter CPB = 100;
 uart_tx #(.CLK_PER_BIT(CPB)) tx(clk, tx_data, tx_data_ready, tx_done, serial_line);
 uart_rx #(.CLK_PER_BIT(CPB)) rx(clk, serial_line, rx_ready, rx_data);
 
-reg [7:0] test_data = 8'b01100011;
-
 integer i;
 initial
 begin
 	tx_data_ready = 0;
 	for(i=0; i<256; i=i+1) begin
-		tx_data = i;
+		tx_data = i[7:0];
 		tx_data_ready = 1;
-		#2;
-		@(posedge clk); tx_data_ready = 0;
+		@(posedge clk);
+		@(posedge clk) tx_data_ready = 0;
 
 		@(posedge rx_ready);
-		#1;
+		@(posedge clk);
 	end
 	$finish;
 end
 always @(posedge rx_ready) begin
 	if (tx_done != 1'b1) begin
 		$display("Error: did not finish transmission");
+		#1 $finish;
 	end
 
 	if (rx_data != tx_data) begin
 		$display("Error: rx_data %h tx_data %h", rx_data, tx_data);
+		#1 $finish;
 	end
 end
 
