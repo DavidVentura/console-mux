30f4a71f71a7de8b5422c9697e7aee125d68555d
DavidVentura <davidventura27@gmail.com>
2023-12-09 16:15:58 +0100

    lint

--- fifo.v
+++ fifo.v
@@ -11,7 +11,7 @@ module fifo #(parameter DEPTH=16, WIDTH=8) (
 
 reg [$clog2(DEPTH)-1:0] w_ptr = 0;
 reg [$clog2(DEPTH)-1:0] r_ptr = 0;
-reg [WIDTH-1:0] fifo_r[DEPTH:0];
+reg [WIDTH-1:0] fifo_r[DEPTH-1:0];
 
 /*
 wire [WIDTH-1:0] dbg_current_r = fifo_r[r_ptr];
@@ -20,29 +20,27 @@ wire [WIDTH-1:0] dbg_zero = fifo_r[0];
 wire [WIDTH-1:0] dbg_one = fifo_r[1];
 */
 
-assign full = (w_ptr == DEPTH-1);
+assign full = ({1'b0, w_ptr} == DEPTH-1);
 assign empty = (r_ptr == w_ptr);
 
 always @(posedge clk) begin
-	// data_out is constantly updated as an optimization;
-	// the value can be read immediately
-	data_out <= fifo_r[r_ptr];
-	if (advance_read_ptr && !empty) begin
-		r_ptr <= r_ptr + 1;
-	end
+	if(empty && w_ptr != 0) begin
+		w_ptr <= 0;
+		r_ptr <= 0;
+		fifo_r[0] <= 0;
+	end else begin
+		// data_out is constantly updated as an optimization;
+		// the value can be read immediately
+		data_out <= fifo_r[r_ptr];
+		if (advance_read_ptr && !empty) begin
+			r_ptr <= r_ptr + 1;
+		end
 
-	if (w_en && !full) begin
-		fifo_r[w_ptr] <= data_in;
-		w_ptr <= w_ptr + 1;
+		if (w_en && !full) begin
+			fifo_r[w_ptr] <= data_in;
+			w_ptr <= w_ptr + 1;
+		end
 	end
-end
-
-always @(posedge empty) begin
-	// Reset the FIFO as soon as we advance the read ptr
-	// beyond the valid values
-	w_ptr <= 0;
-	r_ptr <= 0;
-	fifo_r[0] <= 0;
 end
 
 endmodule
